9286efdc071e77c6f1963c53541ee6cb6ac6cdab
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2019-10-30 12:13:52 +0700

    Change toast show behavior

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/ahmad_fauzan_amirul_isnain/kwiz/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ahmad_fauzan_amirul_isnain/kwiz/MainActivity.java
@@ -2,18 +2,23 @@ package id.ac.ui.cs.mobileprogramming.ahmad_fauzan_amirul_isnain.kwiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NetworkChangeReceiver networkChangeReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        registerReceiver(new NetworkChangeReceiver(), new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver,
+                new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
 
         if (findViewById(R.id.activityMain)!= null) {
             if (savedInstanceState == null) {
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/ahmad_fauzan_amirul_isnain/kwiz/NetworkChangeReceiver.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ahmad_fauzan_amirul_isnain/kwiz/NetworkChangeReceiver.java
@@ -5,24 +5,32 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Log;
 import android.widget.Toast;
 
 public class NetworkChangeReceiver extends BroadcastReceiver {
 
+    private Toast toast;
+
     @Override
     public void onReceive(final Context context, final Intent intent) {
+        if (this.toast != null){
+            toast.cancel();
+        }
+
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         if (activeNetwork != null) {
             if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) {
-                Toast.makeText(context, "Wifi enabled", Toast.LENGTH_LONG).show();
-
+                toast = Toast.makeText(context, "Wifi enabled", Toast.LENGTH_LONG);
+                toast.show();
             } else if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE) {
-                Toast.makeText(context, "Mobile data enabled", Toast.LENGTH_LONG).show();
-
+                toast = Toast.makeText(context, "Mobile data enabled", Toast.LENGTH_LONG);
+                toast.show();
             }
         } else {
-            Toast.makeText(context, "No internet is available", Toast.LENGTH_LONG).show();
+            toast = Toast.makeText(context, "No internet is available", Toast.LENGTH_LONG);
+            toast.show();
         }
     }
 }
