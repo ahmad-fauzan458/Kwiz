d84c2834f1e6eff2fd968ffdb9b0e5e274b74240
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2019-10-31 12:51:09 +0700

    Implements file provider to share medal

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ahmad_fauzan_amirul_isnain/kwiz/WriteStoragePermissions.java
@@ -0,0 +1,34 @@
+package id.ac.ui.cs.mobileprogramming.ahmad_fauzan_amirul_isnain.kwiz;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
+
+public class WriteStoragePermissions {
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+}
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/ahmad_fauzan_amirul_isnain/kwiz/quiz/QuizResultFragment.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ahmad_fauzan_amirul_isnain/kwiz/quiz/QuizResultFragment.java
@@ -1,24 +1,41 @@
 package id.ac.ui.cs.mobileprogramming.ahmad_fauzan_amirul_isnain.kwiz.quiz;
 
-
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import id.ac.ui.cs.mobileprogramming.ahmad_fauzan_amirul_isnain.kwiz.R;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
+import id.ac.ui.cs.mobileprogramming.ahmad_fauzan_amirul_isnain.kwiz.R;
+import id.ac.ui.cs.mobileprogramming.ahmad_fauzan_amirul_isnain.kwiz.WriteStoragePermissions;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class QuizResultFragment extends Fragment {
+
+    private String medalGold;
+    private String medalSilver;
+    private String medalBronze;
 
     public static QuizResultFragment newInstance() {
         return new QuizResultFragment();
@@ -41,5 +58,56 @@ public class QuizResultFragment extends Fragment {
                 getActivity().finish();
             }
         });
+
+        WriteStoragePermissions.verifyStoragePermissions(getActivity());
+
+        medalGold = createImageOnData(R.drawable.medal_gold);
+        medalSilver = createImageOnData(R.drawable.medal_silver);
+        medalBronze = createImageOnData(R.drawable.medal_bronze);
+
+        getView().findViewById(R.id.share_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO Complete medal selection logic
+                onShareMedal(medalGold);
+            }
+        });
+    }
+
+    /**
+     * Create an image on the phone's SD card to later be able to share it.
+     *
+     * @param resID resource ID of an image coming from the res folder.
+     * @return Return the path of the image that was created on the phone SD card.
+     */
+    private String createImageOnData(int resID) {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), resID);
+        String path = Environment.getExternalStorageDirectory() + "/" + resID + ".jpg";
+        File file = new File(path);
+        try {
+            OutputStream out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d("AHEM", "AHEM");
+        }
+
+        return file.getPath();
+    }
+
+    private void onShareMedal(String medal) {
+        Uri path = FileProvider.getUriForFile(getActivity(),
+                "com.id.ac.ui.cs.mobileprogramming.ahmad_fauzan_amirul_isnain.kwiz.fileprovider",
+                new File(medal));
+
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "I got a medal on Kwiz");
+        shareIntent.putExtra(Intent.EXTRA_STREAM, path);
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        shareIntent.setType("image/*");
+        startActivity(Intent.createChooser(shareIntent, "Share..."));
     }
 }
